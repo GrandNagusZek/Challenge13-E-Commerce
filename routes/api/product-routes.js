0b85d962d533dc40b3006f2cca411717b73b82f9
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-04-25 00:37:53 -0400

    added code to handle CRUD operations managing products with associated categories and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,6 +15,41 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Category and Tag data
 });
 
+
+
+// http:localhost:3001/api/catergories/
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category }, { model: Tag,through: ProductTag} ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http:localhost:3001/api/categories/1
+
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model:Category},{ model: Tag, through: ProductTag } ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No reader found with that id!'});
+      return;
+    }
+    
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
