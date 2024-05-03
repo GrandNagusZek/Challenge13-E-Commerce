caff152f50a8961ae708a1e81508b8f3ca022d91
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-05-03 03:08:40 -0400

    added code for api delete route in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 
-// http:localhost:3001/api/catergories/
+// http:localhost:3001/api/products/
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-//http:localhost:3001/api/categories/1
+//http:localhost:3001/api/products/1
 
 router.get('/:id', async (req, res) => {
   try {
@@ -114,8 +114,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
